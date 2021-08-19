468f1b428a498db37baa0fe7bd869b154501a153
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-19 11:55:31 +0400

    sz

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [cancat,setCancat] = useState([1,2,3]);
-
+  const [cancat, setCancat] = useState([1, 2, 3]);
+  const f = () => {
+    setCancat(cancat.concat("55"));
+  };
   return (
-    <div className="App">
-      <div>
+    <div className="App" >
+      <div className="App1">
         {cancat.map((item) => (
-          <div style={{border: "1px solid red",margin:"10px", widows:"60px",height:"40px"}}>{item}</div>
+          <div
+            style={{
+              border: "1px solid red",
+              margin: "10px",
+              width: "80px",
+              height: "40px",
+            }}
+          >
+            {item}
+          </div>
         ))}
       </div>
-     <button> + </button>    
+      <button onClick={f}> Click me + </button>
     </div>
   );
 }
