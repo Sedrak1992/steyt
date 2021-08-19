99b3614f16e1c70aa0a8d7e7580060afefa500c6
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-19 10:40:35 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [cancat,setCancat] = useState([1,2,3]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {cancat.map((item) => (
+          <div style={{border: "1px solid red",margin:"10px", widows:"60px",height:"40px"}}>{item}</div>
+        ))}
+      </div>
+     <button> + </button>    
     </div>
   );
 }
