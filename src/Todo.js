d556a84ec18e007a197cd5ea28e85adbb3e8f51b
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-26 14:31:25 +0400

    y

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,18 @@
-function Todo({item,id,deleteTodo}) {
-    
+import CreateIcon from "@material-ui/icons/Create";
+import IconButton from "@material-ui/core/IconButton";
+
+function Todo({ item, id, deleteTodo }) {
   return (
     <div>
-      <div className="todo" style={{background: item.color}}>
+      <div className="todo" style={{ background: item.color }}>
         <div>{item.name}</div>
         <div>
           <button onClick={() => deleteTodo(id)}> X </button>
+        </div>
+        <div>
+          <IconButton>
+            <CreateIcon />
+          </IconButton>
         </div>
       </div>
     </div>
