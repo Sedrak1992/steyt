0dce956dad5213b788c812cb6f17380a1887b951
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-26 10:01:28 +0400

    aa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
+// import React, { useState } from "react";
+
+// const Form = () => {
+//   const [inputValue, setInputValue] = useState("");
+//   return (
+//     <div>
+//       <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+//       <p>{inputValue}</p>
+//       <button
+//         disabled={inputValue.length === 0}
+//         type="button"
+//         onClick={async () => {
+//           console.log(inputValue);
+//           try {
+//           } catch (e) {
+//             console.log("Sending error", e);
+//           }
+//         }}
+//         style={{ padding: 20, backgroundColor: "red", cursor: "pointer" }}
+//       >
+//         Send to ....
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Form;
+
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [cancat, setCancat] = useState([1, 2, 3]);
+  const [inputValue, setInputValue] = useState("");
+
   const deleteItem = (i) => {
     setCancat((currentItems) =>
       currentItems.filter((item, index) => index !== i)
     );
   };
-const f = (event) => {
-    setCancat(cancat.concat([event.target.value]));
+  const f = (event) => {
+    if (inputValue !== "") {
+      setCancat(cancat.concat([inputValue]));
+      setInputValue("");
+    } else {
+      alert('enter input value')
+    }
   };
 
   return (
@@ -26,22 +61,24 @@ const f = (event) => {
               justifyContent: "space-around",
               alignItems: "center",
             }}
+            key={i}
           >
-            <div>{item}</div>
+            <div> {item}</div>
             <div>
               <button onClick={() => deleteItem(i)}> X </button>
             </div>
           </div>
         ))}
       </div>
-    <button onClick={f} onChange={f} style={{ margin: "20px auto" }}>
-        {" "}
-        Click me +{" "}
-      </button>
-      <input type="text" value={f}/>
+      <button onClick={f}> Add ++</button>
+      <div style={{ margin: "20px auto", display: "babel" }}>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
-
 
 export default App;
